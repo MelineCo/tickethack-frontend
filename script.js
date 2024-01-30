7e04235102ef353c6bbb4e23651d9e24263f436f
Emeline <emeline.courcelle@gmail.com>
2024-01-30 15:24:41 +0100

    cart page style

--- script.js
+++ script.js
@@ -3,11 +3,11 @@ document.querySelector('#search-btn').addEventListener('click', function(){
     .then((trips) =>{
         for (let i = 0; i < trips.length; i++) {
             document.querySelector('#trips').inneHTML += `
-            <div class=${trips[i]._id} class="trip-container">
+            <div class="${trips[i]._id} trip-container">
                 <div>${trips[i].departure} > ${trips[i].arrival}</div>
                 <div>${trips[i].time}</div>
                 <div>${trips[i].price}€</div>
-                <input class="book-btn" type="button" value="Book" />
+                <input class="delete-btn" type="button" value="Book" />
             </div>`
             // Utiliser Moment.js pour récupérer l'heure
            }
