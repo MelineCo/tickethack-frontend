9044a7817cedd5e8bc6a0bfda8bb04c22ec17908
Antoine <antoine.beppo@gmail.com>
2024-01-31 12:10:41 +0100

    scrpitsjs

--- script.js
+++ script.js
@@ -5,24 +5,72 @@ document.querySelector('#btn-search').addEventListener('click', function(){
     const arrival = document.querySelector('#search-arrival').value;
     const date = document.querySelector('#search-date').value;
 
+    console.log(date)
+
     console.log(newTrip = {departure, arrival, date})  
+
+    console.log(`http://localhost:3000/trips/${departure}/${arrival}/${date}`)
     
-    fetch(`http://localhost:3000/trips/${departure}/${arrival}/${date}`).then((response) => response.json())
+    fetch(`http://localhost:3000/trips/${departure}/${arrival}/${date}`)
+      .then((response) => response.json())
       .then((data) => {
-            console.log(data)
-      
-      for (let i = 0; i < trips.length; i++) {
-        document.querySelector('#bookings-container').innerHTML += `
-        <div class="${trips[i]._id} trip-container">
-            <div>${trips[i].departure} > ${trips[i].arrival}</div>
-            <div>${trips[i].time}</div>
-            <div>${trips[i].price}€</div>
-            <input class="delete-btn" type="button" value="Book" />
-        </div>`
-      
-       }
-    })
+        console.log(data)
+        // Vérifier si trajet existe sinon renvoyer logo Trip not found.
+        if (!data) {
+        document.querySelector('#bookings-container').innerHTML = `
+             <div id="bookings-container">
+                <img class="logo" src="images/notfound.png">
+                <h4 class="logo-text">No trips found.</h4>
+            </div>
+        `
+       } else { // Si trajet trouvé creer liste des trajets dans bookings containers
+        console.log(data.Alltrips.length)
+        document.querySelector('#bookings-container').innerHTML = ''; 
+    
+        for (let i = 0; i < data.Alltrips.length; i++) {
+            // console.log(data.Alltrips[i]._id)
+              document.querySelector('#bookings-container').innerHTML += `
+                 <div class="${data.Alltrips[i]._id} trip-container">
+                    <div>${data.Alltrips[i].departure} > ${data.Alltrips[i].arrival}</div>
+                    <div>${data.Alltrips[i].date.split('T')[1].substring(0, 5)}</div>
+                    <div>${data.Alltrips[i].price}€</div>
+                    <button type="button" class="btn-book" id='${data.Alltrips[i]._id}'>Book</button>
+                </div>`;
+            // pour chaque trajet trouvé ajouter ecouteur d'évement sur bouton
+          
+        }
+        buttonAddListener()
+}
 });
+})
 
-
+// fonction écouteur d'évenement bouton 
+function buttonAddListener() {
+    for (let i = 0; i < document.querySelectorAll('.btn-book').length; i++) {
+        document.querySelectorAll('.btn-book')[i].addEventListener('click',
+        
+       // créer fonction du bouton pour créer nouveau document dans collection Booking avec status is-Paid : False
+        function () {
+            console.log(this.id)
+        
+    //             const newBooking = {
+    //             departure: ``,
+    //             arrival: ``,
+    //             date: ``,
+    //             price: ``,
+    //             isPaid: false,
+    //             }
+    //             fetch('http://localhost:3000/bookings/new', {
+    //                 method: 'POST',
+    //                 headers: { 'Content-Type': 'application/json' },
+    //                 body: JSON.stringify(newBooking)
+    //                 })
+    //                 .then(response => response.json())
+    //                 .then(data => {
+    //                 console.log('New trip added to Booking', data);
+    //             });   
+       }
+       )
+    };
+}
 
